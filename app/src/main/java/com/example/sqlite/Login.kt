c186e65f0c2bdfa75efe4b1bcdebc4793c70f440
Rasec <rasec.camacho19@gmail.com>
2020-11-23 11:44:03 -0700

    .

--- app/src/main/java/com/example/sqlite/Login.kt
+++ app/src/main/java/com/example/sqlite/Login.kt
@@ -27,70 +27,73 @@ class Login : AppCompatActivity(){
             startActivity<AddActivity>()
         }
 
-          buttonIngresar.setOnClickListener{
-
-              val values: ArrayList<String>
-              var Usuario:String?=null
-              var password:String?=null
-              Usuario=editTextUsuario.toString()
-              textViewIntent.text=edad.toString()
-              values.add(Usuario)
-              values.add("")
+        buttonIngresar.setOnClickListener {
 
 
-              if(id == 0){
-                  val ID = baseDatos.insert(values)
-                  val ID = baseDatos.query(values)
-                  if(ID>0) {
-                      Toast.makeText(this, "El registro se ingreso con exito", Toast.LENGTH_LONG).show()
-                      finish()
-                  }else{
-                      Toast.makeText(this,"El registro no se realizo!",Toast.LENGTH_LONG).show()
-                  }
-              }else{
-                  val selectionArgs = arrayOf(id.toString())
 
-                  val ID = baseDatos.actualizar(values,"ID=?",selectionArgs)
 
-                  if(ID>0){
-                      Toast.makeText(this,"El registro se realizo con exito",Toast.LENGTH_LONG).show()
-                      finish()
-                  }else{
-                      Toast.makeText(this,"El registro no se realizo con exito",Toast.LENGTH_LONG).show()
-                  }
-          }
+                var intent = Intent(this,PantallaPrincipal::class.java)
+                startActivity(intent)
 
+
+
+            /*if (id == 0) {
+                val ID = baseDatos.insert(values)
+                val ID = baseDatos.query(values)
+                if (ID > 0) {
+                    Toast.makeText(this, "El registro se ingreso con exito", Toast.LENGTH_LONG)
+                        .show()
+                    finish()
+                } else {
+                    Toast.makeText(this, "El registro no se realizo!", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                val selectionArgs = arrayOf(id.toString())
+
+                val ID = baseDatos.actualizar(values, "ID=?", selectionArgs)
+
+                if (ID > 0) {
+                    Toast.makeText(this, "El registro se realizo con exito", Toast.LENGTH_LONG)
+                        .show()
+                    finish()
+                } else {
+                    Toast.makeText(this, "El registro no se realizo con exito", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }*/
+
+
+        }
+
+
+
+
+
+
+
+
+
+        class UsuariAdapter(contexto: Context, var ListaUsuarios: ArrayList<Usuarios>) :
+            BaseAdapter() {
+
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                TODO("Not yet implemented")
+            }
+
+
+            override fun getCount(): Int {
+                return ListaUsuarios.size
+            }
+
+            override fun getItem(position: Int): Any {
+                return ListaUsuarios[position]
+            }
+
+            override fun getItemId(position: Int): Long {
+                return position.toLong()
+            }
+        }
 
     }
-
-
-
-
-
-
-
-
-
-    class UsuariAdapter(contexto: Context, var ListaUsuarios: ArrayList<Usuarios>): BaseAdapter() {
-
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            TODO("Not yet implemented")
-        }
-
-
-        override fun getCount(): Int {
-            return ListaUsuarios.size
-        }
-
-        override fun getItem(position: Int): Any {
-            return ListaUsuarios[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-    }
-
-
 }
